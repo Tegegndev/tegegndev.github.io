aacbf4662a01aded4690753921945a16af220a02
Tegegn wukianos <tegegn.wukianostafese2017eng@wsu.edu.et>
2026-05-08 10:23:37 +0100

    Refactor preloader functionality into separate script file for improved organization

---
+++ script.js
@@ -0,0 +1,11 @@
+    window.addEventListener("load", function () {
+      const preloader = document.getElementById("preloader");
+      setTimeout(function () {
+        document.body.classList.remove("is-loading");
+        if (!preloader) return;
+        preloader.classList.add("hide");
+        setTimeout(function () {
+          preloader.remove();
+        }, 650);
+      }, 1000);
+    });
